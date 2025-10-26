c8c54dce047c29ddf26219289f7a86390ffe7166
sabiu Lawali Tsafe <mobiledev@pi141.com>
2025-10-26 20:15:57 +0100

    Base url changed

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/riders/';
+    public string $baseURL = 'http://localhost/shehubk_riders/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
